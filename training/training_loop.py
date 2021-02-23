e1f3c289732287d8f301fa74ce78128480e66e7b
hlatif <hlatif@color-sensing.com>
2021-02-23 19:13:16 +0100

    fix

--- training/training_loop.py
+++ training/training_loop.py
@@ -407,7 +407,7 @@ def training_loop(
             walltime = timestamp - start_time
             wandb.log({**stats_dict,
                        **{f"Metrics/{key}" for key, value in stats_metrics.items()},
-                       **{"Fake images": image_grid(images, drange=[-1,1], grid_size=grid_size)}})
+                       **{"Fake images": wandb.Image(image_grid(images, drange=[-1,1], grid_size=grid_size))}})
             for name, value in stats_dict.items():
                 stats_tfevents.add_scalar(name, value.mean, global_step=global_step, walltime=walltime)
             for name, value in stats_metrics.items():
